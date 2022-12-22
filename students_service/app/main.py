13c4a36a5ffd1d7d2bc39a5a85e16593f9962773
Xuan Canh <xuancanhit99@gmail.com>
2022-12-22 07:23:54 +0300

    Pr10 finished

--- students_service/app/main.py
+++ students_service/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 # from students_service.app.student import Student, CreateStudentModel  # for IDE Pycharm
 
 from app.student import Student, CreateStudentModel  # for Docker
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 students: list[Student] = [
     # Student("0", "Xuan Canh", "23", "95", "100", "99"),
@@ -22,6 +23,47 @@ def add_student(context: CreateStudentModel):
 
 
 app = FastAPI()
+
+##########
+# Jaeger
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+resource = Resource(attributes={
+    SERVICE_NAME: "stu-service"
+})
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="jaeger",
+    agent_port=6831,
+)
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(jaeger_exporter)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+FastAPIInstrumentor.instrument_app(app)
+
+#
+##########
+
+##########
+# Prometheus
+
+from prometheus_fastapi_instrumentator import Instrumentator
+
+
+@app.on_event("startup")
+async def startup():
+    Instrumentator().instrument(app).expose(app)
+
+
+#
+##########
 
 
 @app.get("/v1/stu")
